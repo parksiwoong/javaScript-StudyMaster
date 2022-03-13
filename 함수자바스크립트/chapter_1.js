ad327c825cb2b10ba5fefb868b0214c7f869b709
parksiwoong <tldnd5656@naver.com>
2022-03-13 16:11:58 +0900

    함수자바스크립트

---
+++ 함수자바스크립트/chapter_1.js
@@ -0,0 +1,16 @@
+var users = [
+    {id: 1 , name : "ID", age: 32},
+    {id: 2 , name : "HA", age: 25},
+    {id: 3,name: "BJ", age: 32},
+    {id: 4 , name : "PJ", age: 28},
+    {id: 5 , name : "JE", age: 27},
+    {id: 6,name: "JM", age: 32},
+    {id: 6,name: "HI", age: 24}
+];
+//30살 보다 적은사람들을 구하는 함수
+var temp_users = [];
+for(var i =0, len = users.length; i < len; i++){
+    if(users[i].age < 30){
+        temp_users.push(users[i]);
+    };
+}
