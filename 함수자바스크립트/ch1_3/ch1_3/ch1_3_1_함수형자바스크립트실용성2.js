e9f9bbeb8a720d84e062fb20eae1159b9bb66a87
parksiwoong <tldnd5656@naver.com>
2022-03-16 22:51:38 +0900

    필터

---
+++ 함수자바스크립트/ch1_3/ch1_3/ch1_3_1_함수형자바스크립트실용성2.js
@@ -0,0 +1,58 @@
+var users = [
+    {id: 1 , name : "ID", age: 32},
+    {id: 2 , name : "HA", age: 25},
+    {id: 3,name: "BJ", age: 32},
+    {id: 4 , name : "PJ", age: 28},
+    {id: 5 , name : "JE", age: 27},
+    {id: 6,name: "JM", age: 32},
+    {id: 6,name: "HI", age: 24}
+];
+function filter(list, predicate){
+    var new_list = [];
+    for(var i =0, ren = list.length; i < ren; i++){
+        if(predicate(list[i])){
+         new_list.push(list[i]);
+        }
+    }
+
+}
+/*console.log(
+    filter(users, function(user){return user.id == 3})[0]
+);*/
+
+
+//이름 찾아서 넣을때 이게 제일 좋은 방법인거 같음 원하는 user를 찾는방법
+var user;
+for (var i = 0, len = users.length; i < len; i++){
+ if(users[i].id==3){
+     user = users[i];
+     break;
+ }
+}
+console.log(user);
+
+function findById(list, id){
+    for(var i =0, len = list.length; i<len; i++){
+        if(list[i].id == id){
+            return list[i];
+        }
+    }
+}
+console.log(findById(users, 3));
+console.log(findById(users, 5));
+
+//나이로 찾는 함수
+function findByAge(list, age){
+    for(var i = 0, len = list.length; i <len; i++){
+        if(list[i].age == age)return list[i];
+    }
+}
+console.log(findByAge(users, 28));
+
+function findByName(list, name){
+    for(var i =0 , len = list.length; i< len; i++){
+        if(list[i].name == name)return list[i];
+    }
+}
+console.log("이름에 맞는 아이가 들어왔니",findByName(users, 'BJ'));
+
--- 함수자바스크립트/필터방식_1장/화살표함수_6.js
+++ 함수자바스크립트/필터방식_1장/화살표함수_6.js
@@ -14,3 +14,29 @@ console.log(log_length(
 
 var ages = list => map(list, v => v.age);
 var names = list => map(list, v => v.name);
+
+console.log(log_length(ages(filter(users, under_30))));
+console.log(log_length((names(filter(users, over_30)))));
+
+//마지막으로 한번만 고쳐보자
+var bvalues = key => list => map(list, v=> v[key]);
+var ages = bvalues('age');
+var names = bvalues('name');
+
+function bvalues(key){
+    return function(list){
+        return map(list, function(v) {return v[key];});
+    }
+}
+// ----
+var ages = bvalues('age');
+var names = bvalues('name');
+var under_30 = function(u){return u.age < 30;};
+var over_30 = function(u){return u.age>= 30;};
+console.log(log_length(ages(filter(users, under_30))));
+console.log(log_length(names(filter(users, over_30))));
+
+function bvalues(key){
+    var value = bvalue(key);
+    return function(list){return map(list, value);}
+}
