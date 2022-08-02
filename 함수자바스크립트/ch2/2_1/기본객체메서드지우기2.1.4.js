1fb85ee4d2cd46d2be849d7dc725f43968b0d1f9
parksiwoong <tldnd5656@naver.com>
2022-08-02 22:17:49 +0900

    함수관련

--- 함수자바스크립트/ch2/2_1/기본객체메서드지우기2.1.4.js
+++ 함수자바스크립트/ch2/2_1/기본객체메서드지우기2.1.4.js
@@ -1,3 +1,4 @@
+//코드 2-12 기본객체의 메서드 지우기
 var obj = {a:1, b:2 , c:3, length: 6};
 delete obj.a;
 delete obj['b']
@@ -6,4 +7,31 @@ console.log(obj);
 
 delete Array.prototype.push;
 var arr1 = [1,2,3];
-arr1.push(4);
+//arr1.push(4);
+
+//코드 2-13 일반적인 함수 정의
+function add1(a,b){
+    return a + b;
+}
+var add2 =function(a,b){
+    return a + b;
+};
+var m = {
+    add3: function(a,b){
+        return a+b
+    }
+}
+    //hi();
+//코드 2-16 선언한 적 없는 변수 참조하기
+//var a = hi;
+
+//코드 2-17 실행하지 않고 참조만 해보기
+console.log(add1);
+console.log(add2); // undefined
+
+function add1(a,b){
+    return a+b;
+}
+var add2 = function (a,b ){
+    return a+ b;
+};
---
+++ 함수자바스크립트/ch2/2_1/호이스팅활용_2.2.3.js
@@ -0,0 +1,94 @@
+//코드 2-18 호이스팅을 이용하여 return문 아래 함수 선언하기
+function add(a,b) {
+    return valid() ? a + b : new Error();
+
+    function valid(){
+        return Number.isInteger(a) && Number.isInteger(b);
+    }
+}
+
+console.log(add(10,5)) // 15;
+console.log(add(10,"a")) //Error
+
+//코드 2-19 호이스팅을 이용해 코드의 순서를 이해하기 편하게 배치
+app.post('/login', function(req, res){
+    db.select("users", { where: { email: req.body.email}}, function(err, user){
+        function end(user){
+            req.session.user = user;
+            res.redirect('/');
+        }
+        if(user && user.password === req.body.password) return end(user);
+
+        db.insert("users", {
+            email: req.body.email,
+            password: req.body.password
+        },function (err, user){
+            end(user);
+        })
+    })
+})
+
+add.post('/login', function (req, res){
+    db.select("users", {where: {email: req.body.email}},function (err, user){
+
+        if(user && user.password === req.body.password) return end(user);
+
+        db.insert("users", {
+            email: req.body.email,
+            password : req.body.password
+        }, function (err, user){
+            end(user);
+        });
+        function end(user){
+            req.session.user = user;
+            res.redirect('/');
+        }
+    })
+})
+
+//2.2.4 괄호없이 즉시 실행하기
+//코드 2-20 일반적인 즉시 실행 방식
+    (function(a) {
+        console.log(a);
+        //100
+    })(100);
+
+//에러난 경우
+function(e){
+    console.log(e)
+}(100);
+
+//코드2-25 괄호 없이 정의가 가능한(즉시 실행도 가능한) 다양한 상황
+!function(a){
+    console.log(a) //1
+}(1);
+
+true && function(a){
+    console.log(a); //1
+}(1);
+
+1 ? function(a){
+    console.log(a); //1
+}(1) : 5;
+
+0, function(a){
+    console.log(a); //1
+}(1);
+
+var b = function(a){
+    console.log(a);
+}(1);
+
+function f2(){}
+f2(function(a){
+    console.log(a) //1
+}(1));
+
+
+var f3 = function c(a){
+    console.log(a);
+}(1);
+
+new function(){
+    console.log(1);
+}
